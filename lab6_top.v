b5488d85138e42f35f2204e9fcabef68b511d9bb
melsafi1 <56850296+melsafi1@users.noreply.github.com>
2019-10-21 16:59:57 -0700

    Update lab6_top.v

--- lab6_top.v
+++ lab6_top.v
@@ -142,6 +142,30 @@ module sseg(in,segs);
   //            14 | E
   //            15 | F
 
-  assign segs = 7'b0001110;  // this will output "F" 
+   always@(*)begin  //remember 0 means open
+case(in)
+	   //6543_210
+4'b0000:segs=7'b1000_000;		//0
+4'b0001:segs=7'b1111_001;		//1
+4'b0010:segs=7'b0100_100;		//2
+4'b0011:segs=7'b0110_000;		//3
+4'b0100:segs=7'b0011_001;		//4
+	   //6543_210
+4'b0101:segs=7'b0010_010;		//5
+4'b0110:segs=7'b0000_010;		//6
+4'b0111:segs=7'b1111_000;		//7
+4'b1000:segs=7'b0000_000;		//8
+	   //6543_210
+4'b1001:segs=7'b0010_000;		//9
+4'b1010:segs=7'b0001_000;		//10=A
+4'b1011:segs=7'b0000_011;		//11=b
+	   //6543_210
+4'b1100:segs=7'b1000_110;		//12=C
+4'b1101:segs=7'b0100_001;		//13=d
+4'b1110:segs=7'b0000_110;		//14=E
+4'b1111:segs=7'b0001_110;		//15F
+default:segs=7'bxxxx_xxx;
+endcase
+end
 
 endmodule
