b2bcb3cac5b8069f0369a035eb38caa353ab9220
melsafi1 <56850296+melsafi1@users.noreply.github.com>
2019-10-21 17:33:19 -0700

    Update MUX_9.v

--- MUX_9.v
+++ MUX_9.v
@@ -1,10 +1,14 @@
 module mux_9(mux_out,mdata,val2,vsel);
 
 	input [15:0] madata,sximm8,C;
-	input []vsel;
+	input [7:0] PC;
+	input [3:0]vsel;
 	output [15:0] mux_out;
 
-	assign mux_out = (vsel == 1'b0) ? val1 : ((vsel == 1'b1) ? val2 : 16'bzzzz_zzzz_zzzz_zzzz);
-					
-
+	case(vsel)
+		0001: mux_out=mdata;
+		0010: mux_out = sximm8; 
+		0100: mux_out= {8'b0,PC};
+		1000: mux_out= C;
+		default:16'bxxxxxxxxxxxxxxxx;
 endmodule
